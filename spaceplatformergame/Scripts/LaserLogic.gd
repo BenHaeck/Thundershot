1241e502276e16a402f7f647a2afcc7ad7577923
BenHaeck <bnhaeckler@gmail.com>
2026-07-12 19:03:33 -0400

    Did some more work on level 1-4
    I added another room, and polished some of the rooms before.

--- spaceplatformergame/Scripts/LaserLogic.gd
+++ spaceplatformergame/Scripts/LaserLogic.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 @onready var lasor_container = $LaserHB
 @onready var laser_point = $LaserEnd
-@onready var rc = $RayCast2D
+@onready var rc: RayCast2D = $RayCast2D
 
 
 
@@ -12,7 +12,9 @@ func _ready() -> void:
 	laser_point.process_mode = Node.PROCESS_MODE_INHERIT
 
 func _physics_process(delta: float) -> void:
-	var dist: float = (rc.get_collision_point() - rc.global_position).length();
+	var dist: float = rc.target_position.length()
+	if rc.is_colliding():
+		dist = (rc.get_collision_point() - rc.global_position).length();
 	laser_point.global_position = rc.get_collision_point()
 	lasor_container.scale.x = dist / 2
 	
